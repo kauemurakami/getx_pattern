d0e8bc859fa9c1960aa10a7aa7a94ccc38695896
Kauê Murakami <kauetmurakami@gmail.com>
2020-06-26 15:59:06 -0300

    atualizando exemplos

--- getx_pattern_example/lib/main.dart
+++ getx_pattern_example/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'app/routes/my_routes.dart';
+import 'app/routes/app_routes.dart';
 import 'app/ui/android/home/home_page.dart';
-import 'app/ui/theme/my_theme.dart';
+import 'app/ui/theme/app_theme.dart';
 
 void main() {
   runApp(
