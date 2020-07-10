52dc60ae9089239c6532062167059b43db099f0e
Kauê Murakami <kauetmurakami@gmail.com>
2020-07-09 21:39:10 -0300

    adicionando imagens sem fundo e classe de internacionalizaçao de forma reutilizavel enquadrada no design pattern

--- getx_pattern_site/lib/app/translations/appTranslations.dart
+++ getx_pattern_site/lib/app/translations/appTranslations.dart
@@ -2,7 +2,7 @@ import 'package:getx_pattern_site/app/translations/en_US/en_US_translation.dart'
 import 'package:getx_pattern_site/app/translations/es_MX/es_mx_translation.dart';
 import 'package:getx_pattern_site/app/translations/pt_BR/pt_br_tranlation.dart';
 
-abstract class Translation {
+abstract class AppTranslation {
   static Map<String, Map<String, String>> 
   translations = 
 {
--- getx_pattern_site/lib/main.dart
+++ getx_pattern_site/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:getx_pattern_site/app/routes/app_pages.dart';
 import 'package:getx_pattern_site/app/theme/app_theme.dart';
+import 'package:getx_pattern_site/app/translations/appTranslations.dart';
 import 'package:getx_pattern_site/app/translations/en_US/en_US_translation.dart';
-import 'package:getx_pattern_site/app/translations/translations.dart';
 
 import 'app/modules/splash/splash_page.dart';
 
@@ -16,9 +16,9 @@ void main() async {
     home: SplashPage(),
     getPages: AppPages.pages,
     theme: appThemeData,
-    
+
     locale: Locale('pt', 'BR'),
-    translationsKeys: Translation.translations,
+    translationsKeys: AppTranslation.translations,
 
     defaultTransition: Transition.rightToLeftWithFade,
   ));
