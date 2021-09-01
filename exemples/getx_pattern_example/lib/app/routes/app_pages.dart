2177967d46a349b055514f894d78e492a81335c1
Nosa Daniel <nosadaniel02@gmail.com>
2021-09-01 17:02:24 +0200

    remove InitialBinding

--- exemples/getx_pattern_example/lib/app/routes/app_pages.dart
+++ exemples/getx_pattern_example/lib/app/routes/app_pages.dart
@@ -1,14 +1,17 @@
-
 import 'package:get/get.dart';
 import 'package:getx_pattern/app/bindings/details_binding.dart';
+import 'package:getx_pattern/app/bindings/home_binding.dart';
 import 'package:getx_pattern/app/ui/android/details/details_page.dart';
 import 'package:getx_pattern/app/ui/android/home/home_page.dart';
 part './app_routes.dart';
 
-
 class AppPages {
   static final pages = [
-    GetPage(name: Routes.INITIAL, page:()=> HomePage(),),
-    GetPage(name: Routes.DETAILS, page:()=> DetailsPage(), binding: DetailsBinding()),
+    GetPage(
+        name: Routes.INITIAL, page: () => HomePage(), binding: HomeBinding()),
+    GetPage(
+        name: Routes.DETAILS,
+        page: () => DetailsPage(),
+        binding: DetailsBinding()),
   ];
 }
