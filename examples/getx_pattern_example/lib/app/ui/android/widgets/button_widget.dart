c907aa789bf13bd5e0d3dd9aa8315f77d13be6a3
unknown <kauetmurakami@gmail.com>
2022-09-22 08:54:31 -0300

    feat: add support micro front end

--- examples/getx_pattern_example/lib/app/ui/android/widgets/button_widget.dart
+++ examples/getx_pattern_example/lib/app/ui/android/widgets/button_widget.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
+
 class RaisedButtonCustomWidget extends StatelessWidget {
   final IconData icon;
   final String text;
   final Function onPressed;
   final Color borderColor;
-  RaisedButtonCustomWidget({@required this.icon, this.text, this.onPressed, this.borderColor = Colors.white });
+  RaisedButtonCustomWidget(
+      {@required this.icon,
+      this.text,
+      this.onPressed,
+      this.borderColor = Colors.white});
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-        color: borderColor,
-        onPressed: this.onPressed,
-        child: Icon(this.icon, color: Colors.white,),);
+    return MaterialButton(
+      color: borderColor,
+      onPressed: this.onPressed,
+      child: Icon(
+        this.icon,
+        color: Colors.white,
+      ),
+    );
   }
 }
